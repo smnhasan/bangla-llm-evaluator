5843e6001f413f5da2793681d47a65bc2cf12edd
s-m-nahid-hasan-nascenia <nahid.hasan@nascenia.com>
2025-08-07 01:32:52 +0600

    added new dependecies

--- setup.py
+++ setup.py
@@ -6,7 +6,21 @@ setup(
     packages=find_packages(),
     install_requires=[
         "pandas>=2.0.0",
-        "numpy>=1.24.0"
+        "numpy>=1.24.0",
+        "llama-cpp-python==0.2.85",
+        "langchain",
+        "langchain_community",
+        "InstructorEmbedding==1.0.1",
+        "sentence-transformers==2.2.2",
+        "transformers>=4.20",
+        "datasets>=2.20",
+        "pyarrow>=17.0",
+        "torch>=2.0",
+        "huggingface-hub==0.24.0",
+        "normalizer @ git+https://github.com/csebuetnlp/normalizer.git",
+    ],
+    dependency_links=[
+        "https://abetlen.github.io/llama-cpp-python/whl/cu122#egg=llama-cpp-python"
     ],
     author="S M Nahid Hasan",
     description="A Bangla LLM evaluation framework",
@@ -15,4 +29,5 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.8",
 )
